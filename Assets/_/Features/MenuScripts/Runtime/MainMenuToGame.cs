2b51ad3b7b788b0689700afc91877e3147304c89
FormatechnoMash <formatechnocite@gmail.com>
2025-06-30 11:22:38 +0200

    MainMenu and GameOver Scenes
    Two new scene with button to change scene just need to put them in the build scene list and add a "MainScene"

---
+++ Assets/_/Features/MenuScripts/Runtime/MainMenuToGame.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace MenuScripts.Runtime
+{
+    public class MainMenuToGame : MonoBehaviour
+    {
+        #region Unity API
+
+        public void LoadScene()
+        {
+            SceneManager.LoadScene("MainScene");  
+        }
+
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
+
+        #endregion
+    }
+}
